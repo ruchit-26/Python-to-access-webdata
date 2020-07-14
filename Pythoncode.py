aad7d606e0e4968305d3f92c15a942fc400d07f1
ruchit-26 <59370121+ruchit-26@users.noreply.github.com>
2020-07-14 16:08:08 +0530

    Update Pythoncode.py
    Python code to find sum of all the integers present in the web page.

--- Pythoncode.py
+++ Pythoncode.py
@@ -1,1 +1,10 @@
+import urllib.request,urllib.parse,urllib.error,re
+fhand=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_648069.html')
+su=0;
+for line in fhand:
+    t=line.decode().strip()
+    x=re.findall('class="comments">([0-9]+)',t)
+    for j in x:
+        su+=int(j)
+print(su)
 
